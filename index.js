fccb8215ca44148750c3b03c7f0ac3860a8dfdf8
ryanhao8369 <ryanhao8369@gmail.com>
2025-06-15 16:05:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1 style={{ color: '#b30000' }}>大里杙福興宮</h1>
+      <p>歡迎參觀福興宮官網。請點選導覽項目進入各功能頁面。</p>
+      <a href="/news">查看最新消息</a>
+    </main>
+  );
+}
